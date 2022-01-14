9e983b709ce4533c6ca85deac9f5e89bd10a49b0
David29-ra <davidrivasa.12@gmail.com>
2022-01-14 18:11:48 -0500

    change name from results to products

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,7 +2,7 @@ class ProductController < ApplicationController
   def index
     products_by_category = Category.all.map do |category|
       results = Product.all.filter { |p| p.category == category.id }
-      { id: category.id, name: category.name, results: results }
+      { id: category.id, name: category.name, products: results }
     end
 
     render json: products_by_category
